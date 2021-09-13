520a4c8f06d7f16573b3ae805d8397719bdb87a0
Matt Brzezinski <matt.brzezinski@invenia.ca>
2021-09-13 16:12:57 -0500

    Added Docker build functionality

--- src/JuliaOnLambda.jl
+++ src/JuliaOnLambda.jl
@@ -5,7 +5,11 @@ using Mocking
 
 @service ECR
 
+const DEFAULT_PROJECT_PATH = joinpath(@__DIR__, "..")
+const DEFAULT_DOCKERFILE = joinpath(DEFAULT_PROJECT_PATH, "Dockerfile")
 const REPO_NOT_FOUND_EXCEPTION = "RepositoryNotFoundException"
+
+include("utilities.jl")
 
 """
 Get the ECR Repo called `repository_name`. If it does not exist, create a new one.
@@ -19,13 +23,29 @@ function _get_create_ecr_repo(repository_name::AbstractString)
     catch e
         if e.code == REPO_NOT_FOUND_EXCEPTION
             @info "Creating ECR repository, $repository_name"
-
             response = @mock ECR.create_repository(repository_name)
             return response["repository"]["repositoryUri"]
         else
             rethrow(e)
         end
     end
+end
+
+"""
+Build and tag the Docker image.
+"""
+function _create_docker_image(
+    repository_name::AbstractString,
+    image::AbstractString,
+    tag::AbstractString;
+    dockerfile_path::AbstractString=DEFAULT_DOCKERFILE,
+)
+    repository_uri = _get_create_ecr_repo(repository_name)
+    image_uri = _image_uri(repository_uri, image, tag)
+
+    _build_docker_image(image_uri; dockerfile_path=dockerfile_path)
+
+    return nothing
 end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -8,6 +8,10 @@ Mocking.activate()
 
 include("patches.jl")
 
+function docker_rmi(name::AbstractString)
+    return read(`docker rmi $(name)`)
+end
+
 @testset "JuliaOnLambda.jl" begin
     @testset "_get_create_ecr_repo" begin
         @testset "repo exists" begin
@@ -32,4 +36,47 @@ include("patches.jl")
             end
         end
     end
+
+    @testset "_build_docker_image" begin
+        image_name = "julia-on-lambda-test-"
+        image_count = 0
+
+        function get_image_name()
+            image_count += 1
+            return string(image_name, image_count)
+        end
+
+        function image_exists(image_name::AbstractString)
+            result = read(`docker images --format "{{.Repository}}"`, String)
+            result = string.(split(result, "\n"))
+
+            return image_name in result
+        end
+
+        @testset "default kwargs" begin
+            try
+                image_name = get_image_name()
+                JuliaOnLambda._build_docker_image(image_name)
+                @test image_exists(image_name)
+            finally
+                docker_rmi(image_name)
+            end
+        end
+
+        @testset "dockerfile_path set" begin
+            dockerfile_path = joinpath(@__DIR__, "resources", "Dockerfile")
+
+            try
+                image_name = get_image_name()
+                JuliaOnLambda._build_docker_image(
+                    image_name; dockerfile_path=dockerfile_path
+                )
+                @test image_exists(image_name)
+            finally
+                docker_rmi(image_name)
+            end
+        end
+    end
+
+    println(read(`docker images`, String))
 end
